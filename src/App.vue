2e5ce667629d824cee3b1391d2b0492f5b3179ed
Simone Pavone <simonebest56@gmail.com>
2022-04-23 15:53:04 +0200

    inserito API

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <NavComp />
+
+    <FilmComp />
+  </div>
+
+
+</template>
+
+<script>
+  import 'bootstrap'
+  import NavComp from './components/NavComp.vue'
+  import FilmComp from './components/FilmComp.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      NavComp,
+      FilmComp
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "bootstrap/dist/css/bootstrap.min.css";
+</style>
---
+++ src/components/FilmComp.vue
@@ -0,0 +1,43 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+    //Importiamo axios
+    import axios from 'axios';
+
+    export default {
+        //Cambiare il nome con quello del componente creato
+        name: 'FilmComp',
+        data() {
+            return {
+                //Creare un array dove salvare i data della chiamata axios
+                //Modificare il nome a piacere
+                FilmsArray: []
+            }
+        },
+        created() {
+            //Qui utiliziamo axios
+            //Modificare il link con l'api che si vuole usare
+            axios.get(
+                    'https://api.themoviedb.org/3/search/movie?api_key=e9b260822f54b240e9160b0ae763ca01&query=Spiderman&language=it-IT'
+                    )
+                .then((res) => {
+                    //Controllo delle informazioni che otteniamo alla chiamata
+                    console.log(res.data.results);
+                    //Modifica dell'array dove salveremo i dati
+                    this.FilmsArray = res.data.results
+                })
+                //Salvataggio in console di possibili errori
+                .catch((error) => {
+                    console.log(error)
+                })
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    /*Inserire style componente*/
+</style>
